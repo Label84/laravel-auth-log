0104f714a14b504c19041d691095a64214256eda
Tjardo <dev@tjardo.com>
2022-01-10 17:45:51 +0100

    Added comments to the config file

--- config/config.php
+++ config/config.php
@@ -1,12 +1,28 @@
 <?php
 
 return [
+    /*
+     * The database table name.
+     */
     'table_name' => 'authentication_logs',
 
+    /*
+     * The database connection for the authentication_logs table.
+     *
+     * Leave unchanged to use the Laravel default.
+     */
     'database_connection' => env('DB_CONNECTION', 'mysql'),
 
+    /*
+     * Enable/Disable the logging to the database.
+     */
     'enabled' => env('AUTH_LOG_ENABLED', true),
 
+    /*
+     * All events that the package will log to the database.
+     *
+     * You can comment out (//) the events that you don't want to log.
+     */
     'events' => [
         \Illuminate\Auth\Events\Attempting::class,
         // \Illuminate\Auth\Events\Authenticated::class,
